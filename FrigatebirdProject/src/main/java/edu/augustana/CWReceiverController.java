1584042f29b6ac5860fd4f52914f69bfe88f50f8
Nathan Sisay <nathansisay22@augustana.edu>
2024-10-13 18:17:08 -0500

    Added CWRecieverController and CWReciever.fxml

---
+++ FrigatebirdProject/src/main/java/edu/augustana/CWReceiverController.java
@@ -0,0 +1,14 @@
+package edu.augustana;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+public class CWReceiverController {
+    @FXML
+    private TextField messageInput;
+    @FXML
+    private TextField frequencyInput;
+    @FXML
+    private TextArea morseDisplay;
+}
