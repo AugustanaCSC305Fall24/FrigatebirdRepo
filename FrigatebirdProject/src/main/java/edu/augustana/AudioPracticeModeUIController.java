b1a213c95d8dd58015d3d0eb561870422f691654
Solyana Kifle <solyanakifle22@augustana.edu>
2024-11-06 23:30:15 -0600

    Added random morse code sound onto the playSound method

--- FrigatebirdProject/src/main/java/edu/augustana/AudioPracticeModeUIController.java
+++ FrigatebirdProject/src/main/java/edu/augustana/AudioPracticeModeUIController.java
@@ -1,13 +1,42 @@
 package edu.augustana;
 
+import edu.augustana.sound.SoundGenerator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-public class AudioPracticeModeUIController {
+public class AudioPracticeModeUIController extends Morse{
+
+    private SoundGenerator randomMorseSound = new SoundGenerator();
+
     @FXML
     public void backToPracticeModeAction(ActionEvent event) throws IOException {
         App.setRoot("PractiseOption");
+    }
+
+    @FXML
+    void playSound(ActionEvent event) {
+        char randomMorseCode = getRandomLetterFromMorseMap();
+        String morseCode = morseMap.get(randomMorseCode);
+        if (morseCode != null){
+            randomMorseSound.playMorseSymbol(morseCode);
+        }
+        System.out.println("playSound button clicked!");
+
+    }
+
+    private char getRandomLetterFromMorseMap() {
+        List<Character> letters = new ArrayList<>(morseMap.keySet());
+        Random random = new Random();
+        return letters.get(random.nextInt(letters.size()));
+    }
+
+    @FXML
+    void initialize() {
+
     }
 }
