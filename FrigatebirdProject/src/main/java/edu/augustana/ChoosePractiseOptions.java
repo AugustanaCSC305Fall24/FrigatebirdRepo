d7b2d7d141337f2f58a8ef93cae6d998297b2b2b
Solyana Kifle <solyanakifle22@augustana.edu>
2024-11-06 22:45:50 -0600

    Added a new fxml where the user chooses if they want to practise audio and visual

---
+++ FrigatebirdProject/src/main/java/edu/augustana/ChoosePractiseOptions.java
@@ -0,0 +1,31 @@
+package edu.augustana;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class ChoosePractiseOptions {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void SwitchToAudio(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SwitchToVisual(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
