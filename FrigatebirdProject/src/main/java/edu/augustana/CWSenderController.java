dee35af0214ec358124bdd8f97b2a77eb3cf5ab4
Kofi <kofiamo-antwi22@augustana.edu>
2024-12-10 22:41:03 -0600

    fixed receiving network

--- FrigatebirdProject/src/main/java/edu/augustana/CWSenderController.java
+++ FrigatebirdProject/src/main/java/edu/augustana/CWSenderController.java
@@ -1,17 +1,9 @@
 package edu.augustana;
 
-import javafx.event.ActionEvent;
+import edu.augustana.Morse;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.VBox;
-
-import java.io.IOException;
 
 public class CWSenderController {
 
@@ -21,55 +13,6 @@ public class CWSenderController {
     private TextField frequencyInput;
     @FXML
     private TextArea morsecodeMessage;
-    @FXML
-    private AnchorPane chatLogBox;
-    private VBox chatLogVBox = new VBox();
-
-
-    @FXML
-    private TextArea chatBox;
-    
-    @FXML
-    private Button backButton;
-
-    private long lastInputTime = System.currentTimeMillis();
-
-    @FXML
-    private TextField morseInput;
-
-
-
-
-    @FXML private void initialize() {
-        frequencyInput.setText("" );
-
-        morseInput.setOnKeyPressed(this::handleMorseInput);
-
-    }
-
-
-    public void handleMorseInput(KeyEvent evt) {
-        long currentTime = System.currentTimeMillis();
-        KeyCode input = evt.getCode();
-        System.out.println("input: **" + input + "**");
-        if (currentTime - lastInputTime > 1000) {
-            morsecodeMessage.appendText(" "); // Add a space if more than 1 second has passed
-        }
-        if (input.equals(KeyCode.DIGIT1)) {
-            morsecodeMessage.appendText(".");
-            double deviation = App.radio.calculateFrequencyDeviation(App.radio.getFrequency());
-            App.radio.getReceivingSoundPlayer().playMorseSymbol(".", deviation);
-        } else if (input.equals(KeyCode.DIGIT2)) {
-            morsecodeMessage.appendText("-");
-            double deviation = App.radio.calculateFrequencyDeviation(App.radio.getFrequency());
-            App.radio.getReceivingSoundPlayer().playMorseSymbol("-", deviation);
-        } else {
-            System.out.println("Invalid input: Only '1' for dit and '2' for dah are allowed.");
-        }
-        lastInputTime = currentTime;
-        morseInput.clear();
-    }
-
 
     @FXML
     public void onTransmitButtonClick() {
@@ -78,32 +21,18 @@ public class CWSenderController {
         double sendersFrequency = Double.parseDouble(frequencyInput.getText());
 
         if (App.radio.canHear(sendersFrequency)) {
-            appendToChatBox("User: " + message);
-            Label messageLabel = new Label("User: " + message);
-            chatLogVBox.getChildren().add(messageLabel);
-
+            // Convert the message to Morse code
             Morse morseConverter = new Morse();
-            String englishMessage = morseConverter.toEnglish(morseCodeText);
-            appendToChatBox("Translated to English: " + englishMessage);
             String morseCode = morseConverter.toMorse(message);
-            morsecodeMessage.setText(morseCode);
 
-
-
-            App.radio.receiveMorseMessage(morseCode, sendersFrequency);
-
-            System.out.println("Message transmitted: " + message);
+            // Create a CWMessage and send it to the server
             CWMessage messageToServer = new CWMessage(morseCode, sendersFrequency);
             App.sendMessageToServer(messageToServer);
+
+            System.out.println("Message transmitted: " + message);
         } else {
             morsecodeMessage.setText("Frequency out of range.");
             System.out.println("Error: Frequency mismatch.");
         }
     }
-    public void appendToChatBox(String message) {
-
-        chatBox.appendText(message + "\n");
-
-    }
-
 }
--- FrigatebirdProject/src/main/java/edu/augustana/sound/ReceiverController.java
+++ FrigatebirdProject/src/main/java/edu/augustana/sound/ReceiverController.java
@@ -1,6 +1,7 @@
 package edu.augustana.sound;
 
 import edu.augustana.App;
+import edu.augustana.CWMessage;
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextArea;
@@ -26,9 +27,10 @@ public class ReceiverController {
         App.setRoot("HomePage");
     }
 
-
     @FXML
     private void initialize() {
+        App.setReceiverController(this); // Register this controller with App
+
         volumeSlider.valueProperty().addListener((obs, oldVal, newVal) -> {
             System.out.println("Volume Slider Value: " + newVal);
         });
@@ -36,5 +38,18 @@ public class ReceiverController {
         frequencySlider.valueProperty().addListener((obs, oldVal, newVal) -> {
             System.out.println("Frequency Slider Value: " + newVal);
         });
+    }
+
+    public void displayReceivedMessage(CWMessage message) {
+        String morseMessage = message.getMorseMessage();
+        double frequency = message.getFrequency();
+
+        // Append the message to the receivedMessageArea
+        receivedMessageArea.appendText(
+                "From Frequency: " + frequency + "\nMorse Code: " + morseMessage + "\n\n"
+        );
+
+        // Optionally: Play the Morse code as sound
+        App.radio.receiveMorseMessage(morseMessage, frequency);
     }
 }
